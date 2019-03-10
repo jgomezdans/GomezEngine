f7d7e2b09cb2580c6718c54033740b0a21c5a9fd
Jose Gomez-Dans <jgomezdans@gmail.com>
2019-03-10 23:21:39 +0000

    Some minor tarting-ups of the code

--- GomezEngine/main.py
+++ GomezEngine/main.py
@@ -20,7 +20,6 @@ from osgeo import gdal
 gdal.UseExceptions()
 
 
-
 def retrieve_field(k, band, url0, roi=None, urlcloud=None, cld_thresh=20):
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
@@ -29,21 +28,26 @@ def retrieve_field(k, band, url0, roi=None, urlcloud=None, cld_thresh=20):
                 prefix = "/vsicurl/"
             else:
                 prefix = ""
-            kwds={"cutlineDSName":f"{prefix:s}{roi}",
-                              "cropToCutline":True}
+            kwds = {"cutlineDSName": f"{prefix:s}{roi}", "cropToCutline": True}
         else:
             kwds = {}
         g1 = gdal.Warp("", f"/vsicurl/{url0:s}", format="MEM", **kwds)
         if urlcloud is not None:
             height = g1.RasterYSize
             width = g1.RasterXSize
-            g = gdal.Warp("", f"/vsicurl/{urlcloud:s}", format="MEM",
-                          height=height, width=width, **kwds)
+            g = gdal.Warp(
+                "",
+                f"/vsicurl/{urlcloud:s}",
+                format="MEM",
+                height=height,
+                width=width,
+                **kwds,
+            )
             cld = g.ReadAsArray()
             if np.sum(cld < cld_thresh) == 0:
                 # No clear pixels
-                return k, band, None        
-        data1 = g1.ReadAsArray()*1.
+                return k, band, None
+        data1 = g1.ReadAsArray() * 1.0
         data1[data1 < -9990] = np.nan
         if urlcloud is not None:
             data1[cld > cld_thresh] = np.nan
@@ -52,17 +56,14 @@ def retrieve_field(k, band, url0, roi=None, urlcloud=None, cld_thresh=20):
         return k, band, data1
 
 
-
 class DataStorage(object):
     def __init__(self, url, max_workers=10):
         try:
             remote_file = urllib.request.urlopen(url)
         except urllib.request.HTTPError as e:
-            raise ValueError("Can't connect. Reason " + 
-                                f"{e.msg:s}")
+            raise ValueError("Can't connect. Reason " + f"{e.msg:s}")
         except urllib.request.URLError as e:
-            raise ValueError("Can't connect. Reason " + 
-                                f"{e.reason:s}")        
+            raise ValueError("Can't connect. Reason " + f"{e.reason:s}")
         try:
             tmp_db = json.loads(remote_file.read())
         except json.JSONDecodeError:
@@ -73,6 +74,7 @@ class DataStorage(object):
         ]
         self.data_db = {kk: tmp_db[k] for (k, kk) in dates}
         self.max_workers = max_workers
+
 
 class DataStorageSentinel2(DataStorage):
     def __init__(self, url):
@@ -92,46 +94,61 @@ class DataStorageSentinel2(DataStorage):
             "AOT",
             "TCWV",
         ]
-        
-    def extract_band(self, bands, dates=None, roi=None, use_cloud_mask=True,
-                            cld_thresh=20):
-        if type(bands) != type([]): bands = [bands]
+
+
+    def extract_band(self, bands, dates=None, roi=None,
+                     use_cloud_mask=True, cld_thresh=20):
+        if not isinstance(bands, list):
+            bands = [bands]
         assert all([band in self.valid_bands for band in bands])
-        
+
         sel_dates = list(self.data_db.keys()) if dates is None else dates
-        if type(sel_dates) != type([]): sel_dates = [sel_dates]
-        not_present = list((set(sel_dates).difference(set(
-                self.data_db.keys()))))
-            
-        if len(not_present)  != 0:
-                raise ValueError(f"{str(not_present):s} not present in DB!")
+        if not isinstance(sel_dates, list):
+            sel_dates = [sel_dates]
+        not_present = list(
+            (set(sel_dates).difference(set(self.data_db.keys())))
+        )
+
+        if not_present:
+            raise ValueError(f"{str(not_present):s} not present in DB!")
         with ThreadPoolExecutor(max_workers=self.max_workers) as ex:
             futures = []
             for band in bands:
                 band_loc = self.valid_bands.index(band)
                 for k in sel_dates:
                     if use_cloud_mask:
-                        cld_file = [s for s in self.data_db[k] 
-                                if s.endswith("_CLD.vrt")][0]
+                        cld_file = [
+                            s
+                            for s in self.data_db[k]
+                            if s.endswith("_CLD.vrt")
+                        ][0]
                     else:
                         cld_file = None
-                    futures.append(ex.submit(retrieve_field, k,
-                                             band, self.data_db[k][band_loc],
-                                             roi, urlcloud=cld_file, 
-                                             cld_thresh=cld_thresh))
+                    futures.append(
+                        ex.submit(
+                            retrieve_field,
+                            k,
+                            band,
+                            self.data_db[k][band_loc],
+                            roi,
+                            urlcloud=cld_file,
+                            cld_thresh=cld_thresh,
+                        )
+                    )
             kwargs = {
-                'total': len(futures),
-                'unit': 'it',
-                'unit_scale': True,
-                'leave': True
+                "total": len(futures),
+                "unit": "it",
+                "unit_scale": True,
+                "leave": True,
             }
             analysis_data = defaultdict(dict)
-            #Print out the progress as tasks complete
-            for f in tqdm(as_completed(futures), **kwargs):
-                f0, f1, f2  = f.result()
-                analysis_data[f1][f0] = f2
+            # Print out the progress as tasks complete
+            for result in tqdm(as_completed(futures), **kwargs):
+                the_date, the_band, the_data = result.result()
+                analysis_data[the_band][the_date] = the_data
         clean_data = {}
         for band in bands:
-            clean_data[band] = {k:v for k,v in analysis_data[band].items() 
-                                if v is not None}
+            clean_data[band] = {
+                k: v for k, v in analysis_data[band].items() if v is not None
+            }
         return clean_data
